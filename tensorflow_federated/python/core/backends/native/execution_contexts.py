e6e1c40794dc2262733288b0782e44f608f970bf
Michael Reneer <michaelreneer@google.com>
2022-02-02 09:06:16 -0800

    Adds a sync and async C++ execution context.
    PiperOrigin-RevId: 425898883

--- tensorflow_federated/python/core/backends/native/execution_contexts.py
+++ tensorflow_federated/python/core/backends/native/execution_contexts.py
@@ -13,13 +13,17 @@
 # limitations under the License.
 """Execution contexts for the native backend."""
 
-from typing import Sequence
+from typing import Optional, Sequence
 
 from tensorflow_federated.python.core.backends.native import compiler
 from tensorflow_federated.python.core.backends.native import mergeable_comp_compiler
 from tensorflow_federated.python.core.impl.context_stack import context_stack_impl
+from tensorflow_federated.python.core.impl.context_stack import set_default_context
+from tensorflow_federated.python.core.impl.execution_contexts import async_execution_context
 from tensorflow_federated.python.core.impl.execution_contexts import mergeable_comp_execution_context
 from tensorflow_federated.python.core.impl.execution_contexts import sync_execution_context
+from tensorflow_federated.python.core.impl.executor_stacks import cpp_executor_factory
+from tensorflow_federated.python.core.impl.executors import executor_bindings
 from tensorflow_federated.python.core.impl.executors import executor_factory
 from tensorflow_federated.python.core.impl.executors import executor_stacks
 
@@ -149,3 +153,94 @@ def set_mergeable_comp_execution_context(
   context = create_mergeable_comp_execution_context(
       executor_factories=executor_factories)
   context_stack_impl.context_stack.set_default_context(context)
+
+
+def set_local_cpp_execution_context(
+    default_num_clients: int = 0,
+    max_concurrent_computation_calls: Optional[int] = None):
+  context = create_local_cpp_execution_context(
+      default_num_clients=default_num_clients,
+      max_concurrent_computation_calls=max_concurrent_computation_calls)
+  set_default_context.set_default_context(context)
+
+
+def create_local_cpp_execution_context(
+    default_num_clients: int = 0,
+    max_concurrent_computation_calls: Optional[int] = None):
+  """Creates a local execution context backed by TFF-C++ runtime.
+
+  Args:
+    default_num_clients: The number of clients to use as the default
+      cardinality, if thus number cannot be inferred by the arguments of a
+      computation.
+    max_concurrent_computation_calls: The maximum number of concurrent calls to
+      a single computation in the CPP runtime. If `None`, there is no limit.
+
+  Returns:
+    An instance of `context_base.Context` representing the TFF-C++ runtime.
+  """
+  factory = cpp_executor_factory.local_cpp_executor_factory(
+      default_num_clients=default_num_clients,
+      max_concurrent_computation_calls=max_concurrent_computation_calls)
+  context = sync_execution_context.SyncSerializeAndExecuteCPPContext(
+      executor_factory=factory,
+      compiler_fn=compiler.desugar_and_transform_to_native)
+  return context
+
+
+def create_local_async_cpp_execution_context(
+    default_num_clients: int = 0,
+    max_concurrent_computation_calls: Optional[int] = None):
+  """Creates a local execution context backed by TFF-C++ runtime.
+
+  Args:
+    default_num_clients: The number of clients to use as the default
+      cardinality, if thus number cannot be inferred by the arguments of a
+      computation.
+    max_concurrent_computation_calls: The maximum number of concurrent calls to
+      a single computation in the CPP runtime. If `None`, there is no limit.
+
+  Returns:
+    An instance of `context_base.Context` representing the TFF-C++ runtime.
+  """
+  factory = cpp_executor_factory.local_cpp_executor_factory(
+      default_num_clients=default_num_clients,
+      max_concurrent_computation_calls=max_concurrent_computation_calls)
+  context = async_execution_context.AsyncSerializeAndExecuteCPPContext(
+      executor_factory=factory,
+      compiler_fn=compiler.desugar_and_transform_to_native)
+  return context
+
+
+def set_remote_cpp_execution_context(
+    channels: Sequence[executor_bindings.GRPCChannel],
+    default_num_clients: int = 0):
+  context = create_remote_cpp_execution_context(
+      channels=channels, default_num_clients=default_num_clients)
+  set_default_context.set_default_context(context)
+
+
+def create_remote_cpp_execution_context(
+    channels: Sequence[executor_bindings.GRPCChannel],
+    default_num_clients: int = 0
+) -> sync_execution_context.SyncSerializeAndExecuteCPPContext:
+  """Creates a remote execution context backed by TFF-C++ runtime."""
+  factory = cpp_executor_factory.remote_cpp_executor_factory(
+      channels=channels, default_num_clients=default_num_clients)
+  context = sync_execution_context.SyncSerializeAndExecuteCPPContext(
+      executor_factory=factory,
+      compiler_fn=compiler.desugar_and_transform_to_native)
+  return context
+
+
+def create_remote_async_cpp_execution_context(
+    channels: Sequence[executor_bindings.GRPCChannel],
+    default_num_clients: int = 0
+) -> async_execution_context.AsyncSerializeAndExecuteCPPContext:
+  """Creates a remote execution context backed by TFF-C++ runtime."""
+  factory = cpp_executor_factory.remote_cpp_executor_factory(
+      channels=channels, default_num_clients=default_num_clients)
+  context = async_execution_context.AsyncSerializeAndExecuteCPPContext(
+      executor_factory=factory,
+      compiler_fn=compiler.desugar_and_transform_to_native)
+  return context
